d2df9cdcd87d3741d41b34a204f61ca1d0b6343e
artyom-kalman <p.a.kalashnikov@ya.ru>
2024-10-31 11:12:08 +0900

    chat gpt request

--- internal/cafeteria/chatgpt-service.go
+++ internal/cafeteria/chatgpt-service.go
@@ -13,16 +13,22 @@ import (
 	"github.com/artyom-kalman/kbu-daily-menu/internal/cafeteria/entities"
 )
 
-const MENU_PROMPT = "Опиши эти корейские блюда. Для каждого блюда напиши одно предложение. Также напиши степень остроты блюда. Вот список блюд: "
+const MENU_PROMPT = "Опиши блюда. Ответ дай в виде json: name: название на русском, description: описание, spiciness: степень остроты от 1 до 5. Вот список блюд: "
 
-func AddDescriptionToMenu(menu *entities.Menu) error
-	for _, item := range menu.Items {
-		item.Description = "Суповая версия ттокпокки с острыми рисовыми клецками в бульоне."
+func AddDescriptionToMenu(menu *entities.Menu) error {
+	prompt := generatePromtForMenu(menu)
+
+	items, err := sendRequest(prompt)
+	if err != nil {
+		return err
 	}
+
+	menu.Items = items
+
 	return nil
 }
 
-func formMenuPrompt(menu *entities.Menu) string {
+func generatePromtForMenu(menu *entities.Menu) string {
 	question := strings.Clone(MENU_PROMPT)
 	for _, item := range menu.Items {
 		question += fmt.Sprintf("%s, ", item.Name)
@@ -30,18 +36,20 @@ func formMenuPrompt(menu *entities.Menu) string {
 	return question
 }
 
-func sendRequest(prompt string) (*entities.GPTResponse, error) {
-	apiKey := os.Getenv("OPEN_AI_API_KEY")
+func sendRequest(prompt string) ([]*entities.MenuItem, error) {
+	apiKey := os.Getenv("AIML_API_KEY")
 	if apiKey == "" {
-		return nil, errors.New("set env key for openai")
+		return nil, errors.New("set env key for AI/ML")
 	}
 
 	reqBody := entities.GPTRequest{
-		Model: "gpt-4",
+		Model: "gpt-4o",
 		Messages: []*entities.Message{{
 			Role:    "user",
 			Content: prompt,
 		}},
+		MaxToken: 1024,
+		Stream:   false,
 	}
 
 	reqBodyJson, err := json.Marshal(reqBody)
@@ -51,7 +59,7 @@ func sendRequest(prompt string) (*entities.GPTResponse, error) {
 
 	req, err := http.NewRequest(
 		"POST",
-		"https://api.openai.com/v1/chat/completions",
+		"https://api.aimlapi.com/chat/completions",
 		bytes.NewBuffer(reqBodyJson),
 	)
 	if err != nil {
@@ -67,18 +75,46 @@ func sendRequest(prompt string) (*entities.GPTResponse, error) {
 		return nil, err
 	}
 	defer res.Body.Close()
-	println(res.Status)
+
+	if res.StatusCode != 200 && res.StatusCode != 201 {
+		body, _ := io.ReadAll(res.Body)
+		println(string(body))
+		return nil, errors.New("Bad request")
+	}
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	var resBody entities.GPTResponse
-	err = json.Unmarshal(body, &resBody)
+	var gptResponse entities.GPTResponse
+	err = json.Unmarshal(body, &gptResponse)
 	if err != nil {
 		return nil, err
 	}
 
-	return &resBody, nil
+	return ParseRespons(gptResponse.Choices[0].Message.Content)
+}
+
+func ParseRespons(res string) ([]*entities.MenuItem, error) {
+	// if !isReponseJson(res) {
+	// 	return nil, errors.New("error parsing response from AI")
+	// }
+
+	jsonString := strings.ReplaceAll(res, "\n", "")
+	jsonString = strings.ReplaceAll(jsonString, "\t", "")
+	jsonString = jsonString[7 : len(jsonString)-3]
+	println(jsonString)
+
+	var items []*entities.MenuItem
+	err := json.Unmarshal([]byte(jsonString), &items)
+	if err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
+
+func isReponseJson(rep string) bool {
+	return strings.HasPrefix(rep, "```json") && strings.HasSuffix(rep, "```")
 }
--- internal/cafeteria/entities/chatgpt-request.go
+++ internal/cafeteria/entities/chatgpt-request.go
@@ -3,4 +3,6 @@ package entities
 type GPTRequest struct {
 	Model    string     `json:"model"`
 	Messages []*Message `json:"messages"`
+	Stream   bool       `json:"stream"`
+	MaxToken int        `json:"max_token"`
 }
