cbbe7b5fad45dfd7ac440757d1f231fb28232e9f
Artyom <108268804+artyom-kalman@users.noreply.github.com>
2024-11-05 12:44:50 +0900

    LLM support  (#4)
    * api not working
    
    * tailwind build
    
    * add .env file
    
    * replace log with panic
    
    * add Dockerfile
    
    * refactor
    
    * refactor
    
    * refactor
    
    * api not working
    
    * add tests
    
    * change names of images
    
    * remove api endpoints
    
    * chat gpt request
    
    ---------
    
    Co-authored-by: artyom-kalman <p.a.kalashnikov@ya.ru>

--- internal/cafeteria/entities/menu-item.go
+++ internal/cafeteria/entities/menu-item.go
@@ -3,4 +3,13 @@ package entities
 type MenuItem struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
+	Spiciness   int    `json:"spiciness"`
+}
+
+func (i *MenuItem) AddDescription(description string) {
+	i.Description = description
+}
+
+func (i *MenuItem) AddSpiciness(spiciness int) {
+	i.Spiciness = spiciness
 }
